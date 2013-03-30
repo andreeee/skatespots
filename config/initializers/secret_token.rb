27a9514907dfa67ba35542d6172a8094202b6a51
Andre Williams <andrewilliamsyo@gmail.com>
2013-03-30 15:04:42 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skatespots::Application.config.secret_token = '7374ecd8375c07ea9f29d9276831031dad6c72cf353114f47550c0535dc5b511ab2645be2e818434d1b152f5581f0fdbe48f6b33ec829492644a5f3e59037963'
